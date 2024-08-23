9f2338b5d1784a72f5a7633a8d95329dc37b059f
onaingo <ingo95864@gmail.com>
2024-08-23 13:05:45 -0600

    Remove all files except flattenedSource.txt

--- src/components/Toast.js
+++
@@ -1,9 +0,0 @@
-
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-const showToast = () => {
-    toast.success("This is a toast notification!");
-};
-
-export { ToastContainer, showToast };
